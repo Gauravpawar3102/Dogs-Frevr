7852458c5afb0edfc23c5f53deda8b85b220b8b8
Gaurav <gauravpawar3102@gmail.com>
2022-01-21 08:14:33 +0530

    info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,7 +6,7 @@ function Info() {
       <div className="infoText ml-8  text-4xl font-mono font-semibold">
         Know Your Dog
       </div>
-      <div className="infoContainer flex p-4 justify-around">
+      <div className="infoContainer flex  gap-8 p-4 justify-around">
         <Infocard />
         <Infocard />
         <Infocard />
@@ -17,13 +17,13 @@ function Info() {
 const Infocard = () => {
   return (
     <>
-      <div className="infoCard-container  shadow-2xl  w-fit  ">
+      <div className="infoCard-container grow shadow-2xl  w-fit  ">
         <img
           className="info-img rounded-t-lg object-scale-down h-56 w-full"
           src={image}
           alt="Img"
         />
-        <div className="infoCard-info flex flex-col  bg-red-200  capitalize gap-1 rounded-t-lg font-mono -translate-y-1 p-4">
+        <div className="infoCard-info flex flex-col items-center bg-red-200  capitalize gap-1 rounded-t-lg font-mono -translate-y-1 p-4">
           <div className="infoCard-name font-bold">{`German Shepard`}</div>
           <div className="infoCard-origin">{`Germany`}</div>
           <div className="infoCard-height">{`50Cm`}</div>
