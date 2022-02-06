fce6242fe33e3893b129ba53f229f241ef7fc901
Gaurav <gauravpawar3102@gmail.com>
2022-02-06 13:47:00 +0530

    added card

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,7 @@ import profileImage from '../assets/hero-img.jpg';
 function ProfileCard() {
   return (
     <div className="Profile-container w-fit  border-4 border-sky-500">
-      <img className="h-40 w-44 object-contain" src={profileImage} alt="img" />
+      <img className="h-52 w-full object-cover " src={profileImage} alt="img" />
       <ProfileCardInfo />
     </div>
   );
@@ -11,14 +11,18 @@ function ProfileCard() {
 
 const ProfileCardInfo = () => {
   return (
-    <div className="info-container">
-      <div className="breedName text-2xl ">{`Breed Name : German Shepard`}</div>
-      <div className="origin text-2xl">{`Origin : Germany`}</div>
-      <div className="height text-2xl">{`Height :55cm-60cm`}</div>
-      <div className="weight text-2xl">{`Weight :55kg-60kg`}</div>
-      <div className="coat text-2xl">{`Coat : Thick`}</div>
+    <div className="info-container p-4">
+      <div className="breedName text-l ">{`Breed Name : German Shepard`}</div>
+      <div className="origin text-l">{`Origin : Germany`}</div>
+      <div className="height text-l">{`Height :55cm-60cm`}</div>
+      <div className="weight text-l">{`Weight :55kg-60kg`}</div>
+      <div className="coat text-l">{`Coat : Thick`}</div>
     </div>
   );
+};
+
+const profileIcons = () => {
+  return <div className="container"></div>;
 };
 
 export default ProfileCard;
