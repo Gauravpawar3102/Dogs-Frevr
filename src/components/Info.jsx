84f6da659085da9c3e9f887964732578209ad402
Gaurav <gauravpawar3102@gmail.com>
2022-02-14 22:23:32 +0530

    gk

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -27,7 +27,7 @@ function Info() {
 const Infocard = () => {
   const [bName, setBName] = useState('German jshepr');
   const [bImg, setbImg] = useState('img');
-  const [origin, setOrigin] = useState('Gemany');
+  const [origin, setOrigin] = useState('Geml;any');
   const [height, setHeight] = useState('55cm-60cm');
   const [weight, setWeight] = useState('56kg-65kg');
   return (
